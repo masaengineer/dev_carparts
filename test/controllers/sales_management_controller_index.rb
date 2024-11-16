7c745a46166045bdf123d8132c7456a64a503f60
masaengineer <masahiro.track.a.f@gmail.com>
2024-11-17 00:48:35 +0900

    売上管理画面の実装、rails-ujsのインポート

---
+++ test/controllers/sales_management_controller_index.rb
@@ -0,0 +1,8 @@
+require "test_helper"
+
+class SalesManagementControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get sales_management_index_url
+    assert_response :success
+  end
+end
