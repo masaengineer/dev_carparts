21b92d0b6ffb4c0f4f790b92ff44147b198b36f2
masaengineer <masahiro.track.a.f@gmail.com>
2024-11-17 15:06:09 +0900

    deviseコントローラーの実装、erbフォーマッターのインストール

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  # Deviseのルーティングを最初に配置
   devise_for :users, controllers: {
     sessions: 'users/sessions'
   }
-
   resources :landing, only: [:index]
   root "sales_management#index"
 end
