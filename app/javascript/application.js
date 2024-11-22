6f0cde34d11c8f24b662f6ea51efcb9ccf3c21d4
masaengineer <masahiro.track.a.f@gmail.com>
2024-11-22 17:43:14 +0900

    rails-ujsを削除

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,4 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-// rails-ujs のインポートと初期化
-import Rails from "@rails/ujs"
-Rails.start()
+Turbo.start()
